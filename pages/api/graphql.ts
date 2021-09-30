df1296dc7e4ba7d220df25cf8f11c5e06349b3c8
bue221 <camiloplaza3@gmail.com>
2021-09-30 00:19:04 -0500

    feat: init config antdesign and graphql config

---
+++ pages/api/graphql.ts
@@ -0,0 +1,28 @@
+import  {  ApolloServer  }  from  "apollo-server-micro";
+import   typeDefs    from  "./schemas";
+import   resolvers  from  "./resolvers";
+import { PageConfig } from 'next';
+import { MicroRequest } from 'apollo-server-micro/dist/types';
+import { ServerResponse } from 'http';
+import { ApolloServerPluginLandingPageGraphQLPlayground } from 'apollo-server-core';
+
+export const config: PageConfig = {
+  api: {
+    bodyParser: false,
+  },
+};
+
+const apolloServer = new ApolloServer({
+  typeDefs,
+  resolvers,
+  plugins: [ApolloServerPluginLandingPageGraphQLPlayground()],
+});
+
+const startServer = apolloServer.start();
+
+export default async function handler(req: MicroRequest, res: ServerResponse) {
+  await startServer;
+  await apolloServer.createHandler({
+    path: '/api/graphql',
+  })(req, res);
+}
