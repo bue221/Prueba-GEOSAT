4c7023050ff1512f545eaa57b2562d1b9f0ae3b9
bue221 <camiloplaza3@gmail.com>
2021-10-05 03:26:27 -0500

    resolve bug

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -4,11 +4,8 @@ let prisma: PrismaClient;
 
 if (process.env.NODE_ENV === 'production') {
   prisma = new PrismaClient();
-} else {
-  if (!global.prisma) {
-    global.prisma = new PrismaClient();
-  }
-  prisma = global.prisma;
+} else {  
+    prisma = new PrismaClient();
 }
 
 export default prisma;
